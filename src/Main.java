87c255be3796f515ee59378e90f7b9167bc67c4f
Niko test <Lesnik6.6.6@mail.ru>
2023-05-17 14:29:42 +0300

    Change

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 public class Main {
 
     public static void main(String[] args) {
-        
+        int amount = 12400;
+        int bonusMiles = amount / 20;
+        System.out.println("Начисленные мили:" + bonusMiles);
     }
 }
